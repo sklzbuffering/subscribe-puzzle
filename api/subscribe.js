53f18a6cef90593324b5c86f2644fd5a234ccf7a
Simon <sklindo32@gmail.com>
2023-08-01 00:48:55 -0500

    added subscribe.js serverless function to subscribe-puzzle

---
+++ api/subscribe.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const { email, name } = req.body;
+
+  if (!email || !name) {
+    return res.status(400).json({ message: 'Name and email are required' });
+  }
+
+  try {
+    const response = await axios.post(
+      `https://api.mailerlite.com/api/v2/groups/${process.env.MAILERLITE_GROUP_ID}/subscribers`,
+      {
+        email: email,
+        name: name
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-MailerLite-ApiKey': process.env.MAILERLITE_API_KEY
+        }
+      }
+    );
+
+    return res.status(200).json({ message: 'Subscription successful' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'An error occurred' });
+  }
+};
+
