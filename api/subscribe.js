e4fb37ac4786944d95c7547a6d7c79bcb30b9702
Simon <sklindo32@gmail.com>
2023-08-05 20:53:06 -0500

    Added CORS Headers to Okay Request

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,6 +1,15 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
   if (!req.body) {
     return res.status(400).json({ message: 'Request body is missing' });
   }
