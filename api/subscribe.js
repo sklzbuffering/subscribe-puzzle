95157dba14385e5c0e5ed3a6999c54152893039c
Simon <sklindo32@gmail.com>
2023-08-05 20:23:36 -0500

    Added AJAX  Request Body Missing Error Message

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,47 +1,35 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-  let body = '';
+  if (!req.body) {
+    return res.status(400).json({ message: 'Request body is missing' });
+  }
 
-  // Middleware to parse JSON
-  req.on('data', chunk => {
-    body += chunk.toString(); // convert Buffer to string
-  });
+  const { email, name } = req.body;
 
-  req.on('end', async () => {
-    try {
-      req.body = JSON.parse(body);
-    } catch (err) {
-      console.error('Error parsing JSON', err);
-      return res.status(400).json({ message: 'Bad request' });
-    }
+  if (!email || !name) {
+    return res.status(400).json({ message: 'Name and email are required' });
+  }
 
-    const { email, name } = req.body;
-
-    if (!email || !name) {
-      return res.status(400).json({ message: 'Name and email are required' });
-    }
-
-    try {
-      const response = await axios.post(
-        `https://api.mailerlite.com/api/v2/groups/${process.env.MAILERLITE_GROUP_ID}/subscribers`,
-        {
-          email: email,
-          name: name
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'X-MailerLite-ApiKey': process.env.MAILERLITE_API_KEY
-          }
+  try {
+    const response = await axios.post(
+      `https://api.mailerlite.com/api/v2/groups/${process.env.MAILERLITE_GROUP_ID}/subscribers`,
+      {
+        email: email,
+        name: name
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-MailerLite-ApiKey': process.env.MAILERLITE_API_KEY
         }
-      );
+      }
+    );
 
-      return res.status(200).json({ message: 'Subscription successful' });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'An error occurred' });
-    }
-  });
+    return res.status(200).json({ message: 'Subscription successful' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'An error occurred' });
+  }
 };
 
