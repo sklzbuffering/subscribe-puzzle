390b69482d4fafe320f59c9a9d8cf96ffceb16f8
Simon <sklindo32@gmail.com>
2023-08-08 01:14:59 -0500

    Added Domain Request to CORS

--- api/subscribe.js
+++ api/subscribe.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 module.exports = async (req, res) => {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://mintstylist.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
